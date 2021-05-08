f7b9a49e2564f802da26be949b2e5cd3d6916f2a
Panagiotis Moutsiounas <p18mout@ionio.gr>
2021-05-08 15:50:00 +0300

    Add files via upload

--- project efarmosmenos progammatismos me python/Pong.py
+++ project efarmosmenos progammatismos me python/Pong.py
@@ -1,22 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
+#Pong game
 
 #Import libraries used for this program
 # a basic graphic library
 import turtle
 
-win = turtle.Screen()
-pad_a= turtle.Turtle()
-pad_b= turtle.Turtle()
-ball= turtle.Turtle()
-pen = turtle.Turtle()
-
 def pad_a_up():
     y = pad_a.ycor()
     y += 20
-    pad_a.sety(y)   
+    pad_a.sety(y)
 
 def pad_a_down():
     y = pad_a.ycor()
@@ -26,68 +17,69 @@ def pad_a_down():
 def pad_b_up():
     y = pad_b.ycor()
     y += 20
-    pad_b.sety(y)   
+    pad_b.sety(y)
 
 def pad_b_down():
     y = pad_b.ycor()
     y -= 20
-    pad_b.sety(y)  
+    pad_b.sety(y)
 
-def main():
+win = turtle.Screen()
+win.title("A PING-PONG Game")
+win.bgcolor('black')
+win.setup(width=800,height=600)
+win.tracer(0)
+
+#Left Pad
+pad_a= turtle.Turtle()
+pad_a.speed(0)
+pad_a.shape("square")
+pad_a.color("blue")
+pad_a.shapesize(stretch_wid=5,stretch_len=1)
+pad_a.penup()
+pad_a.goto(-350,0)
+
+# Right Pad
+pad_b= turtle.Turtle()
+pad_b.speed(0)
+pad_b.shape("square")
+pad_b.color("red")
+pad_b.shapesize(stretch_wid=5,stretch_len=1)
+pad_b.penup()
+pad_b.goto(350,0)
+
+# Ball
+ball= turtle.Turtle()
+ball.speed(1000)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+ball.dx = 0.05
+ball.dy = 0.05
+
+# Score
+score_a = 0
+score_b = 0
+
+# score context
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A : "+ str(score_a) +" Player B : "+ str(score_b),align="center",font=("Courier",24,"normal"))
 
 
-    win.title("A PING-PONG Game")
-    win.bgcolor('black')
-    win.setup(width=800,height=600)
-    win.tracer(0)
-
-    #Left Pad
-    pad_a.speed(0)
-    pad_a.shape("square")
-    pad_a.color("blue")
-    pad_a.shapesize(stretch_wid=5,stretch_len=1)
-    pad_a.penup()
-    pad_a.goto(-350,0)
-
-    # Right Pad
-    
-    pad_b.speed(0)
-    pad_b.shape("square")
-    pad_b.color("red")
-    pad_b.shapesize(stretch_wid=5,stretch_len=1)
-    pad_b.penup()
-    pad_b.goto(350,0)
-
-    # Ball
-    
-    ball.speed(1000)
-    ball.shape("square")
-    ball.color("white")
-    ball.penup()
-    ball.goto(0,0)
-    ball.dx = 0.05
-    ball.dy = 0.05
-
-    # Score
-    score_a = 0
-    score_b = 0
-
-    # score context
-    
-    pen.speed(0)
-    pen.color("white")
-    pen.penup()
-    pen.hideturtle()
-    pen.goto(0,260)
-    pen.write("Player A : "+ str(score_a) +" Player B : "+ str(score_b),align="center",font=("Courier",24,"normal"))
+win.listen()
+win.onkeypress(pad_a_up,"w")
+win.onkeypress(pad_a_down,"s")
+win.onkeypress(pad_b_up,"Up")
+win.onkeypress(pad_b_down,"Down")
 
 
-    win.listen()
-    win.onkeypress(pad_a_up,"w")
-    win.onkeypress(pad_a_down,"s")
-    win.onkeypress(pad_b_up,"Up")
-    win.onkeypress(pad_b_down,"Down")
-
+def main(score_a=0, score_b=0):
     while True:
         win.update()
         ball.setx(ball.xcor() + ball.dx)
@@ -123,9 +115,10 @@ def main():
         if (ball.xcor() < -340 and ball.xcor() > -350) and ball.ycor() < pad_a.ycor() + 50 and ball.ycor() > pad_a.ycor() - 50 :
             ball.setx(-340)
             ball.dx *= -1
+
+
+
             
 if __name__ == '__main__':
     main()
-
-
 
